bfabe6b809739a6edcb3b9827b7535ec6cc59b3c
diputs-sudo <diputs-sudo@proton.me>
2025-07-19 10:51:35 -0400

    debug metadata_builder.py

--- metadata_builder.py
+++ metadata_builder.py
@@ -5,10 +5,7 @@ import sys
 import json
 from datetime import datetime
 
-PYTHON_STDLIB = {
-    'os', 'sys', 're', 'math', 'json', 'socket', 'subprocess', 'time',
-    'datetime', 'random', 'base64', 'threading', 'itertools', 'functools'
-}
+PYTHON_STDLIB = {'abc','aifc','argparse','array','ast','asynchat','asyncio','asyncore','atexit','audioop','base64','bdb','binascii','binhex','bisect','bz2','cProfile','calendar','cgi','cgitb','chunk','cmath','cmd','code','codecs','codeop','collections','colorsys','compileall','concurrent','configparser','contextlib','contextvars','copy','copyreg','crypt','csv','ctypes','curses','dataclasses','datetime','dbm','decimal','difflib','dis','distutils','doctest','email','encodings','ensurepip','enum','errno','faulthandler','fcntl','filecmp','fileinput','fnmatch','formatter','fractions','ftplib','functools','gc','getopt','getpass','gettext','glob','graphlib','grp','gzip','hashlib','heapq','hmac','html','http','imaplib','imghdr','imp','importlib','inspect','io','ipaddress','itertools','json','keyword','lib2to3','linecache','locale','logging','lzma','mailbox','mailcap','marshal','math','mimetypes','mmap','modulefinder','msilib','msvcrt','multiprocessing','netrc','nntplib','numbers','operator','optparse','os','ossaudiodev','parser','pathlib','pdb','pickle','pickletools','pip','pipes','pkgutil','platform','plistlib','poplib','posix','posixpath','pprint','profile','pstats','pty','pwd','py_compile','pyclbr','pydoc','pydoc_data','pyexpat','queue','quopri','random','re','readline','reprlib','resource','rlcompleter','runpy','sched','secrets','select','selectors','shelve','shlex','shutil','signal','site','smtpd','smtplib','sndhdr','socket','socketserver','spwd','sqlite3','ssl','stat','statistics','string','stringprep','struct','subprocess','sunau','symtable','sys','sysconfig','syslog','tabnanny','tarfile','telnetlib','tempfile','termios','test','textwrap','threading','time','timeit','tkinter','token','tokenize','trace','traceback','tracemalloc','tty','turtle','turtledemo','types','typing','unicodedata','unittest','urllib','uu','uuid','venv','warnings','wave','weakref','webbrowser','winreg','winsound','wsgiref','xdrlib','xml','xmlrpc','zipapp','zipfile','zipimport','zlib'}
 
 def extract_imports(code):
     tree = ast.parse(code)
